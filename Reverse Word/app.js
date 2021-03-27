a463cf14c74650848fc2d90b70c341638f774a85
Durmuş PALA <71263028+durmus-pala@users.noreply.github.com>
2021-03-27 07:17:17 +0300

    add another method

--- Reverse Word/app.js
+++ Reverse Word/app.js
@@ -1,4 +1,10 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable eqeqeq */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-plusplus */
+/* eslint-disable linebreak-style */
+/* eslint-disable space-infix-ops */
+/* eslint-disable linebreak-style */
 /* eslint-disable keyword-spacing */
 /* eslint-disable linebreak-style */
 /* eslint-disable indent */
@@ -12,10 +18,25 @@
 /* eslint-disable no-tabs */
 /* eslint-disable linebreak-style */
 function reverseWords(message) {
-  if (message.length <= 1) {
-    return message;
+  // if (message.length <= 1) {
+  //   return message;
+  // }
+  //  return (message.split(' ').reverse().join(' '));
+  let str = ''; // sentence
+  let str2 = ''; // reversed sentence
+  for(let i = 0; i<message.length; i++) {
+    // kelime sonu gelene kadar harfleri birleştir
+    if (message[i] != ' ') {
+      str += message[i];
+      // kelime sonu geldiğinde cümleye ekle
+    } else {
+      str2 = str + ' ' + str2;
+      str = '';
+    }
   }
-   return (message.split(' ').reverse().join(' '));
+  str2 = str + ' ' + str2;
+  // döngüde son oluşturulan kelimeye de ekle ve yeniden arraya çevir
+  input = str2.slice(0, -1).split('');
 }
 
 // console.log(reverseWords('clarusway'));
@@ -26,34 +47,38 @@ function reverseWords(message) {
 
 // You can write additional helper functions as well.
 
-/* Tests */
-let desc = 'Don\'t reverse single word';
-let input = 'Clarusway';
-let actual = reverseWords(input);
-let expected = 'Clarusway';
-assertEqual(actual, expected, desc);
+ /* Tests */
+ let desc = 'Don\'t reverse single word';
+ let input = 'Clarusway'.split('');
+ reverseWords(input);
+ let actual = input.join('');
+ let expected = 'Clarusway';
+ assertEqual(actual, expected, desc);
 
-desc = 'Inspring Quote #1';
-input = 'days. big make things Little';
-actual = reverseWords(input);
-expected = 'Little things make big days.';
-assertEqual(actual, expected, desc);
+ desc = 'Inspring Quote #1';
+ input = 'days. big make things Little'.split('');
+ reverseWords(input);
+ actual = input.join('');
+ expected = 'Little things make big days.';
+ assertEqual(actual, expected, desc);
 
-desc = 'Inspring Quote #2';
-input = 'impossible. mean not does hard but hard, be to going It’s';
-actual = reverseWords(input);
-expected = 'It’s going to be hard, but hard does not mean impossible.';
-assertEqual(actual, expected, desc);
+ desc = 'Inspring Quote #2';
+ input = 'impossible. mean not does hard but hard, be to going It’s'.split('');
+ reverseWords(input);
+ actual = input.join('');
+ expected = 'It’s going to be hard, but hard does not mean impossible.';
+ assertEqual(actual, expected, desc);
 
-desc = 'Inspring Quote #3';
-input = 'done. you’re when Stop tired. you’re when stop Don’t';
-actual = reverseWords(input);
-expected = 'Don’t stop when you’re tired. Stop when you’re done.';
-assertEqual(actual, expected, desc);
-function assertEqual(a, b, desc) {
-if (a === b) {
-    console.log(`${desc} ... PASS`);
-}else {
-    console.log(`${desc} ... FAIL: ${a} ≠ ${b}`);
-	}
-}
+ desc = 'Inspring Quote #3';
+ input = 'done. you’re when Stop tired. you’re when stop Don’t'.split('');
+ reverseWords(input);
+ actual = input.join('');
+ expected = 'Don’t stop when you’re tired. Stop when you’re done.';
+ assertEqual(actual, expected, desc);
+ function assertEqual(a, b, desc) {
+ if (a === b) {
+     console.log(`${desc} ... PASS`);
+ }else {
+     console.log(`${desc} ... FAIL: ${a} ≠ ${b}`);
+   }
+ }
